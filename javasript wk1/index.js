dd9a8f1b199570ce889fe15c66149181ba9eed89
Feryalabid <feryalabid123456@gmail.com>
2024-09-20 18:47:03 +0500

    Add files via upload

---
+++ javasript wk1/index.js
@@ -0,0 +1,20 @@
+alert("Feryal Abid");
+
+//no 2
+myname = "Feryal Abid";
+alert ( "Hi! I'm " +myname);
+console.log("My name is " +"  "+ myname)
+
+//no3
+var a = 4
+var b = 2
+var result = (a++ + --b - --a + b++ + a + b);
+console.log("The result of this equation is :" +result)
+
+//no4
+var Chinese = prompt("Is chinese availabe or not?");
+if(Chinese === "available"){
+    alert("Order Chinese");
+} else if(Chinese === "not available");{
+    alert("Order Italian");
+} 
